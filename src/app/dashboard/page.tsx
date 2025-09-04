909b1dd4060d2b0b274be5681470bbdaff7a41c3
BB HG <blackbrotherhacker001@gmail.com>
2025-09-04 19:57:47 +0000

    remove it

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,13 +3,10 @@
 
 import { TradeChart } from "@/components/dashboard/trade-chart";
 import { TradePanel } from "@/components/dashboard/trade-panel";
-import { LandingHeader } from "@/components/landing/landing-header";
 
 
 export default function DashboardPage() {
   return (
-    <>
-    <LandingHeader />
     <div className="flex flex-col md:flex-row h-[calc(100vh-65px)]">
       <div className="flex-grow h-full">
         <TradeChart />
@@ -18,6 +15,5 @@ export default function DashboardPage() {
         <TradePanel />
       </div>
     </div>
-    </>
   );
 }
