e76c9a0ed5f8d19b1577db9e0412a797b9243d93
BB HG <blackbrotherhacker001@gmail.com>
2025-09-06 18:39:31 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,13 @@ import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  projectId: "cryptosim-beur1",
-  appId: "1:625801511335:web:0be6ee94bce680d7413682",
-  storageBucket: "cryptosim-beur1.firebasestorage.app",
-  apiKey: "AIzaSyC2GqTyMJ15kyJ7yeRcaR89jSH-BPrQ-zo",
-  authDomain: "cryptosim-beur1.firebaseapp.com",
-  messagingSenderId: "625801511335",
+  "projectId": "cryptosim-beur1",
+  "appId": "1:625801511335:web:0be6ee94bce680d7413682",
+  "storageBucket": "cryptosim-beur1.firebasestorage.app",
+  "apiKey": "AIzaSyC2GqTyMJ15kyJ7yeRcaR89jSH-BPrQ-zo",
+  "authDomain": "cryptosim-beur1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "625801511335"
 };
 
 // Initialize Firebase
