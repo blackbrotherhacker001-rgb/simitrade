12555db0037e901badf294050096f3711c0d0834
BB HG <blackbrotherhacker001@gmail.com>
2025-09-06 05:33:19 +0000

    show full Tradin table

---
+++ src/app/dashboard/binary-trading/layout.tsx
@@ -0,0 +1,19 @@
+
+'use client';
+
+import { Header } from '@/components/dashboard/header';
+
+export default function BinaryTradingLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="flex-1 bg-[#161A25] overflow-hidden">
+        {children}
+      </main>
+    </div>
+  );
+}
--- src/app/dashboard/binary-trading/page.tsx
+++ src/app/dashboard/binary-trading/page.tsx
@@ -6,18 +6,11 @@ import { TradePanel } from '@/components/dashboard/trade/trade-panel';
 
 export default function BinaryTradingPage() {
   return (
-    <div className="flex flex-1 overflow-hidden">
-        <div className="flex-1 flex flex-col h-full">
-            <div className="flex-grow h-[calc(100%-4rem)]">
-                <TradingViewWidget />
-            </div>
-             <div className="h-16 flex-shrink-0 border-t border-border/60">
-                {/* Could be a footer for open trades */}
-            </div>
-        </div>
-        <div className="w-80 flex-shrink-0 border-l border-border/60">
-            <TradePanel />
-        </div>
+    <div className="relative h-full w-full">
+      <TradingViewWidget />
+      <div className="absolute top-0 right-0 h-full w-80">
+        <TradePanel />
+      </div>
     </div>
   );
 }
